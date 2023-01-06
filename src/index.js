8fcd143f85ce61f424e8f74016a39cf5796c6093
sviatl <sv.yurusova@gmail.com>
2023-01-06 18:13:42 +0300

    feat: function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+    return reverse1(n);
 }
+
+function reverse1(number){
+    number = Math.abs(number);
+    if (number == 0) {return 0};
+    mm = [];
+    while (number > 0) {
+        c = number % 10;
+        mm.push(c);
+        number = (number - c) / 10;
+    }
+    ans = "";
+    mm.forEach(element => {
+        ans = ans + element;
+    });
+
+    return 1*ans;
+}
+
+
